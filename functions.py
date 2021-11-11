28483151d9d7801f75f42a81e54df9aebcc0b76f
Camari Nwokorie <93938410+Camari1@users.noreply.github.com>
2021-11-10 21:37:06 -0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,10 +6,21 @@ def next():
     answer = input("Press \"ENTER\" to continue...")
     if answer == '':
         pass
+    
+# This function is used for items in the story. If a player has an item in the dictionary,
+# the items that they have could be useful in the future.
+item = {}
+def items(name):
+    item[name] = 0 
+    return item
 
+# This will be used for the save function, by appending the player's input of choices to the list.
+save = []
+def save_list(choice_number):
+    save.append(choice_number)
+    return save
+    
 music_list = {1: "assets/Start_Music.mp3", 2: "assets/Odd.mp3"}
-
-
 def play_music(number_list, volume, length=0.0):
     pygame.mixer.music.load(music_list[number_list])
     pygame.mixer.music.set_volume(volume)
